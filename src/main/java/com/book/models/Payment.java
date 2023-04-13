5c1e0c639826b7cfcd5c473c3482422d6f9ebde2
Satbat58 <satishprajapati585858@gmail.com>
2023-04-13 14:59:02 +0530

    first commit

---
+++ src/main/java/com/book/models/Payment.java
@@ -0,0 +1,59 @@
+package com.book.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Payment 
+{
+	@Id
+	@GeneratedValue(generator = "payment_seq")
+	@SequenceGenerator(name = "payment_seq",initialValue = 111111)
+	private int pid;
+	private String userid;
+	private String card;
+	private long totalPayment;
+	private String date;
+	public Payment() {}
+	public Payment(String userid, String card, long totalPayment, String date) 
+	{
+		this.userid = userid;
+		this.card = card;
+		this.totalPayment = totalPayment;
+		this.date = date;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getCard() {
+		return card;
+	}
+	public void setCard(String card) {
+		this.card = card;
+	}
+	public long getTotalPayment() {
+		return totalPayment;
+	}
+	public void setTotalPayment(long totalPayment) {
+		this.totalPayment = totalPayment;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
---
+++ src/main/java/com/book/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.book.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.book.models.BookTransaction;
+
+public interface TransactionRepository extends JpaRepository<BookTransaction,Integer>
+{
+	List<BookTransaction> findByUserid(String userid);
+}
